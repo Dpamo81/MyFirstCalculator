0f5f349a638a8d3d15ba02313ec24da2698507e7
daniel.pajares <dpajares81@gmail.com>
2023-05-31 15:32:11 +0200

    Added initial architecture

---
+++ MyFirstCalculator/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public static int Sum(int value1, int value2)
+        {
+            int result = value1 + value2;
+            return result;
+        }
+        public static int Substraction(int value1, int value2)
+        {
+            int result = value1 - value2;
+            return result;
+        }
+        public static int Multiplication(int value1, int value2)
+        {
+
+            int result = value1 * value2;
+            return result;
+        }
+        public static int Division(int value1, int value2)
+        {
+
+            int result = value1 / value2;
+            return result;
+        }
+    }
+}
---
+++ MyFirstCalculatorTests/CalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+
+        [TestMethod()]
+        public void SumTest()
+        {
+            var calculator = new Calculator();
+            Assert.IsTrue(Calculator.Sum(2, 4) == 6);
+        }
+
+        [TestMethod()]
+        public void SubstractionTest()
+        {
+            var calculator = new Calculator();
+            Assert.IsTrue(Calculator.Substraction(4, 2) == 2);
+        }
+
+        [TestMethod()]
+        public void MultiplicationTest()
+        {
+            var calculator = new Calculator();
+            Assert.IsTrue(Calculator.Multiplication(2, 4) == 8);
+        }
+
+        [TestMethod()]
+        public void DivisionTest()
+        {
+            var calculator = new Calculator();
+            Assert.IsTrue(Calculator.Division(4, 4) == 1);
+        }
+    }
+}
